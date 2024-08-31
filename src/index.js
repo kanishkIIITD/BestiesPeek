82528fdf5dfff0559dc515a802801cabb9d0e1f2
kanishkIIITD <kanishk18044@iiitd.ac.in>
2024-08-31 19:18:00 +0530

    Installed speed insights

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import App from "./App";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -11,6 +12,7 @@ root.render(
         <BrowserRouter>
             <App />
             <Analytics />
+            <SpeedInsights />
         </BrowserRouter>
     </React.StrictMode>
 );
