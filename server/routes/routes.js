aa4e4ffb9fe1c68dab782f77446e8cdd0804b246
kanishkIIITD <kanishk18044@iiitd.ac.in>
2024-09-03 14:17:31 +0530

    Removed youtubecontroller from routes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
-const YouTubeController = require("../controllers/getYoutubeVideos");
+// const YouTubeController = require("../controllers/getYoutubeVideos");
 
 const { getStreamerData } = require("../controllers/getStreams");
 const { getUsers } = require("../controllers/getUsers");
 
 router.post("/getStreamerData", getStreamerData);
-router.get("/youtube/channel/:channelId/videos", YouTubeController.fetchVideos);
+// router.get("/youtube/channel/:channelId/videos", YouTubeController.fetchVideos);
 router.post("/users", getUsers);
 
 module.exports = router;
